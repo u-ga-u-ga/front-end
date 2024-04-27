3439c6e8043677fd52aceaf90a39b4b66ded6449
ring-wdr <akswnd55@gmail.com>
2024-04-27 23:45:16 +0900

    chore: 프론트엔드 초기 환경 설정 #1 tailwindcss && shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,11 @@ export default defineConfig({
           react: ["react", "react-dom"],
         },
       },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
