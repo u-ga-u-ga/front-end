3342ba9d02f3ad700d4e521b785380f4dc7787d7
aliveornot <aliveornot@gmail.com>
2024-05-06 17:03:55 +0900

    register to router

--- src/pages/RegisterItem/index.tsx
+++ src/pages/RegisterItem/index.tsx
@@ -1,9 +1,15 @@
+import { createFileRoute } from "@tanstack/react-router";
+
 import { ItemRegistrationForm } from "@/widgets/forms/itemRegistrationForm";
 import { RegisterItemPageHeader } from "@/widgets/headers";
 
+export const Route = createFileRoute("/RegisterItem/")({
+  component: RegisterItemPage,
+});
+
 export default function RegisterItemPage() {
   return (
-    <div className="flex h-full w-full flex-col gap-6 self-stretch  ">
+    <div className="flex w-full flex-col gap-6 self-stretch  ">
       <RegisterItemPageHeader
         onClickLeftButton={function (): void {
           throw new Error("Function not implemented.");
