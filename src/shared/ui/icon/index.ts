e0bbb60f64f661cb686890b0fc6a6b5f4a59fb2a
aliveornot <aliveornot@gmail.com>
2024-05-15 21:11:43 +0900

    design: BottomSheet for Share

---
+++ src/shared/ui/icon/index.ts
@@ -0,0 +1,13 @@
+export { default as Bag } from "./bag.svg?react";
+export { default as BellOutline } from "./bellOutline.svg?react";
+export { default as Check } from "./check.svg?react";
+export { default as ChevronLeft } from "./chevronLeft.svg?react";
+export { default as Copy } from "./copy.svg?react";
+export { default as Cross } from "./cross.svg?react";
+export { default as DefaultProfile } from "./defaultProfile.svg?react";
+export { default as Google } from "./google.svg?react";
+export { default as Heart } from "./heart.svg?react";
+export { default as Home } from "./home.svg?react";
+export { default as Kakao } from "./kakao.svg?react";
+export { default as Naver } from "./naver.svg?react";
+export { default as Receipt } from "./receipt.svg?react";
