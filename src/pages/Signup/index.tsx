b00095c75660e8f54875090c3f3b713713d4ad56
aliveornot <aliveornot@gmail.com>
2024-05-16 22:22:39 +0900

    fix: signup page routing info

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,7 +1,26 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { SignupPage } from "./SignupPage";
+import { SignupForm } from "@/widgets/forms";
+import { SignUpPageHeader } from "@/widgets/headers";
 
 export const Route = createFileRoute("/Signup/")({
   component: SignupPage,
 });
+
+function SignupPage() {
+  return (
+    <div className="flex w-full flex-1 flex-col gap-6">
+      <SignUpPageHeader
+        onClickLeftButton={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        onClickRightButton={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+      <div className="items-strech flex flex-col px-8 py-6">
+        <SignupForm />
+      </div>
+    </div>
+  );
+}
