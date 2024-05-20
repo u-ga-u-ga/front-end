d608ec1188f9c5a7db7b28800df9582a61839b17
ring-wdr <akswnd55@gmail.com>
2024-05-20 21:29:57 +0900

    refactor: ref를 활용한 scroll값 감지

--- src/widgets/headers/detailPageHeader/index.tsx
+++ src/widgets/headers/detailPageHeader/index.tsx
@@ -1,28 +1,55 @@
-import { HTMLAttributes } from "react";
+import {
+  CSSProperties,
+  HTMLAttributes,
+  RefObject,
+  useDeferredValue,
+} from "react";
 import { Link, useRouter } from "@tanstack/react-router";
 import { Share } from "lucide-react";
 
 import copyTextToClipboard from "@/shared/lib/copyTextToClipboard";
+import useScrollYRef from "@/shared/lib/useScrollYRef";
 import { cn } from "@/shared/lib/utils";
 import ChevronLeft from "@/shared/ui/icon/chevronLeft.svg?react";
 import HomeIcon from "@/shared/ui/icon/home.svg?react";
 
 import styles from "./index.module.css";
 
-type DetailPageHeaderProps = HTMLAttributes<HTMLElement>;
+type DetailPageHeaderProps = HTMLAttributes<HTMLElement> & {
+  _parent?: RefObject<HTMLElement>;
+};
+
+const calculatePosition = (position: number, standard = 1) => {
+  const calculatedScroll =
+    position > window.innerHeight ? 1 : position / window.innerHeight;
+  return Math.floor(calculatedScroll * 255) * standard;
+};
 
 export default function DetailPageHeader({
   className,
+  _parent,
   ...props
 }: DetailPageHeaderProps) {
   const router = useRouter();
+  const scrollY = useScrollYRef(_parent);
+  const deferedPosition = useDeferredValue(scrollY);
+  const calculatedPosition = calculatePosition(deferedPosition, 4);
+
   return (
     <nav
       className={cn(
-        "top-0 z-10 flex w-full justify-between gap-2.5 p-4",
+        "sticky top-0 z-10 flex h-[6.875rem] w-full justify-between gap-2.5 p-4",
         styles.nav,
         className
       )}
+      style={
+        {
+          "--header-color": `rgb(${Array(3)
+            .fill(255 - calculatedPosition)
+            .join(",")})`,
+          "--header-background-opacity": calculatedPosition / 255,
+        } as CSSProperties
+      }
       {...props}
     >
       <div className="relative flex shrink-0 flex-row items-center justify-start gap-2.5">
