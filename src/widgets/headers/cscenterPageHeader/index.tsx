a2beda16ce6de3a02959379060f0d359a5dbea47
ring-wdr <akswnd55@gmail.com>
2024-05-20 21:29:25 +0900

    refactor: header 통합

--- src/widgets/headers/cscenterPageHeader/index.tsx
+++ src/widgets/headers/cscenterPageHeader/index.tsx
@@ -2,35 +2,27 @@ import { useRouter } from "@tanstack/react-router";
 
 import copyTextToClipboard from "@/shared/lib/copyTextToClipboard";
 import { Button } from "@/shared/ui";
-import CommonHeaderComponent_EXPERIMENTAL from "@/shared/ui/header_Experimental";
+import { Type3Header } from "@/shared/ui/headerProptotype/Type3Header";
 import { Share } from "@/shared/ui/icon";
 import ChevronRight from "@/shared/ui/icon/chevronLeft.svg?react";
 
 export default function CSCenterPageHeader() {
   const router = useRouter();
   return (
-    <CommonHeaderComponent_EXPERIMENTAL
-      title={"고객센터"}
-      leftElement={
-        <Button
-          variant={"ghost"}
-          size={"sm"}
-          onClick={router.history.back}
-          role="link"
-        >
+    <Type3Header
+      title="고객센터"
+      leftButton={
+        <Button variant={"ghost"} size={"sm"} role="link">
           <ChevronRight />
         </Button>
       }
-      rightElement={
-        <Button
-          variant={"ghost"}
-          size={"sm"}
-          onClick={() => copyTextToClipboard(window.location.href)}
-          role="link"
-        >
+      onClickLeftButton={router.history.back}
+      rightButton={
+        <Button variant={"ghost"} size={"sm"} role="link">
           <Share />
         </Button>
       }
+      onClickRightButton={() => copyTextToClipboard(window.location.href)}
     />
   );
 }
